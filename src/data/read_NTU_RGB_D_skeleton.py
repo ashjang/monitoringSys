abde1121e901f294e7437eda787dd8a4dcded38c
ashjang <vkdvkdgkfla@naver.com>
2023-06-17 17:06:54 +0900

    feat: 필요없는 코드 삭제

--- src/data/read_NTU_RGB_D_skeleton.py
+++ src/data/read_NTU_RGB_D_skeleton.py
@@ -12,69 +12,6 @@ Three functions are provided.
 import numpy as np
 
 
-# def read_skeleton(file):
-#     with open(file, 'r') as f:
-#         skeleton_sequence = {'numFrame': int(f.readline()), 'frameInfo': []}
-        
-#         for t in range(skeleton_sequence['numFrame']):
-#             # frame_info = {'bodyInfo': []}
-#             frame_info = {'numBody': 1, 'bodyInfo': []}
-#             # print(frame_info['numBody'])
-#             for m in range(frame_info['numBody']):
-#             #     body_info_key = [
-#             #         'bodyID', 'clipedEdges', 'handLeftConfidence',
-#             #         'handLeftState', 'handRightConfidence', 'handRightState',
-#             #         'isResticted', 'leanX', 'leanY', 'trackingState'
-#             #     ]
-#             #     body_info = {
-#             #         k: float(v)
-#             #         for k, v in zip(body_info_key, f.readline().split())
-#             #     }
-#             #     # print(body_info)
-#             #     body_info['numJoint'] = int(f.readline())               # 25
-#             #     # print(body_info['numJoint'])
-#             #     body_info['jointInfo'] = []
-#                 f.readline()
-#                 body_info = {'jointInfo': []}
-#                 for v in range(25):
-#                     joint_info_key = [
-#                         'x', 'y', 'z'
-#                     ]
-#                     joint_info = {
-#                         k: float(v)
-#                         for k, v in zip(joint_info_key, f.readline().split())
-#                     }
-#                     if joint_info != {}:
-#                         body_info['jointInfo'].append(joint_info)
-#                     # print(body_info['jointInfo'])
-#                 frame_info['bodyInfo'].append(body_info)
-#                 # print(body_info)
-#                 # print(frame_info['bodyInfo'])
-#             skeleton_sequence['frameInfo'].append(frame_info)
-#     # print(skeleton_sequence)
-#     return skeleton_sequence
-
-
-# def read_xyz(file, max_body=2, num_joint=25):
-#     seq_info = read_skeleton(file)
-#     # print(seq_info)
-#     data = np.zeros((3, seq_info['numFrame'], num_joint, max_body), dtype=np.float32)
-#     # print(data)
-#     # print(seq_info['frameInfo'])
-#     for n, f in enumerate(seq_info['frameInfo']):
-#         for m, b in enumerate(f['bodyInfo']):
-#             for j, v in enumerate(b['jointInfo']):
-#                 if m < max_body and j < num_joint:
-#                     # print(v['x'], v['y'], v['z'])
-#                     data[:, n, j, m] = [v['x'], v['y'], v['z']]
-#                 else:
-#                     pass
-#     data = np.around(data, decimals=3)
-#     # print(data)
-
-#     return data
-
-
 def read_skeleton(file):
     with open(file, 'r') as f:
         # shape = eval(f.readline())
